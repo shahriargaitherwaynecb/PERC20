93c20566cb3675218c96c6d93818ffd5dac7d0eb
shahriargaitherwaynecb <shahriargaitherwaynecbo@hotmail.com>
2024-09-22 09:03:23 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("9689dc1c625eafa33f7d3b744c3bb54b6663a2d2df864797099600b9195a62d3", "9689dc1c625eafa33f7d3b744c3bb54b6663a2d2df864797099600b9195a62d3") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
